574934c9fe86e50429b6020391995d88aeffb166
PupilTong <earth.diqiu@gmail.com>
2019-10-19 23:30:13 -0400

    Update patient.js

--- patient.js
+++ patient.js
@@ -1,18 +1,25 @@
 // JavaScript Document
 function clickMe(index,painLevel){
-	var s = localStorage.getItem("storage");
+	var s = JSON.parse(localStorage.getItem("storage"));
 	s[index]=painLevel;
 	localStorage.setItem("storage",s);
+	document.getElementById(index).innerHTML="";
 }
 function onLoad(){
-	var painArea = document.getElementsByClassName("painArea");
+	var painAreaList = document.getElementsByClassName("painArea");
 	var dict = {};
-	for(var i=0; i < painArea.length; i++){
-		dict[painArea[i].id] = 0;//Intinital Pain Score
+	for(var i=0; i < painAreaList.length; i++){
+		dict[painAreaList[i].id] = 0;//Intinital Pain Score
 	}
 	localStorage.setItem("storage", JSON.stringify(dict));
 	return 1;
 }
 function selectPainLevel(id){
-	
+	document.getElementById(id).innerHTML("<div></div>");
+}
+function loadStoraged(){
+	var s = JSON.parse(localStorage.getItem("storage"));
+	for(var element in s){
+		document.getElementById(element).!!!!!!addsomecolores""//balabala
+	}
 }
